945225af89aa4f47726d570bda40f720fe5b9fb0
Fermin Martinez <159151419+ferminmartineez@users.noreply.github.com>
2026-09-24 17:42:16 +0200

    Refactor Firebase configuration setup
    Removed Firebase SDK imports and initialization code, and set the Firebase configuration in the window object.

--- config.js
+++ config.js
@@ -1,21 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyAoTuMwHU-3DW5FQH0Div3gqUlQ6Gj7lDI",
   authDomain: "gastos-fermin.firebaseapp.com",
   projectId: "gastos-fermin",
   storageBucket: "gastos-fermin.firebasestorage.app",
   messagingSenderId: "558000419206",
-  appId: "1:558000419206:web:cf1cfa672790965bda7c07",
-  measurementId: "G-92ZMYLG40S"
+  appId: "1:558000419206:web:cf1cfa672790965bda7c07"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
